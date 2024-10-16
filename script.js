0572ffea91f15de1b33c4e83e523a23f0902fd82
Anas Zughayyar <anaszughayyar@neobook.local>
2024-10-16 22:25:16 +0300

    new repo

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Page loaded....");
+
+
+function add_like_1() {
+    var x = document.getElementById("likes-number-1");
+    x.textContent++;
+}
+
+function add_like_2() {
+    var x = document.getElementById("likes-number-2");
+    x.textContent++;
+}
+
+function add_like_3() {
+    var x = document.getElementById("likes-number-3");
+    x.textContent++;
+}
